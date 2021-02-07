fce38557dd2dd530e71f55b372fb70a16c0e398c
Kayana Nusa <tony.arianto@gmail.com>
2021-02-07 17:52:04 +0700

    insert to DB

--- app/Imports/JadwalImport.php
+++ app/Imports/JadwalImport.php
@@ -2,9 +2,13 @@
 
 namespace App\Imports;
 
+use App\Models\trs_jadwal;
 use Illuminate\Support\Collection;
+use Illuminate\Support\Facades\DB;
 use Maatwebsite\Excel\Concerns\ToCollection;
 use Maatwebsite\Excel\Concerns\WithHeadingRow;
+
+use function PHPUnit\Framework\isNull;
 
 class JadwalImport implements ToCollection, WithHeadingRow
 {
@@ -25,15 +29,24 @@ class JadwalImport implements ToCollection, WithHeadingRow
                         $pin = $value2;
                     }
                     if($no > 2) {
-                        $data[$pin][$key2]['pin'] = $pin;
-                        $data[$pin][$key2]['tgl'] = gmdate("Y-m-d", (($key2 - 25569) * 86400));
-                        $data[$pin][$key2]['jadwal'] = $value2;
+                        // $data[$pin][$key2]['pin'] = $pin;
+                        // $data[$pin][$key2]['tgl'] = gmdate("Y-m-d", (($key2 - 25569) * 86400));
+                        // $data[$pin][$key2]['jadwal'] = $value2;
+
+                        //DB::insert('insert into trs_jadwal (pin, tgl, jadwal) values (?, ?, ?)', [$pin, gmdate("Y-m-d", (($key2 - 25569) * 86400)), $value2]);
+                        trs_jadwal::create([
+                            'code' => $pin . gmdate("Ymd", (($key2 - 25569) * 86400)),
+                            'pin' => $pin,
+                            'tgl' => gmdate("Y-m-d", (($key2 - 25569) * 86400)),
+                            'jadwal' => $value2,
+
+                        ]);
                     }
                     $no++;
-
                 }
         }
-        dump($data);
+
+        //trs_jadwal::where('jadwal', isNull())->delete();
     }
 
     public function headingRow(): int
